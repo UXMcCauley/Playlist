8383d4dc574fc9bb1245ac849053f4d055930b07
drewfsc <uxmccauley@gmail.com>
2025-03-14 22:34:57 -0500

    Refine styles and integrate Silkscreen font.
    Updated CSS for a cleaner and more minimal aesthetic, including adjustments to borders, backgrounds, and opacity for better visual clarity. Integrated the Silkscreen font into the app and redesigned visual components like the LCD screen for consistent branding. Simplified JSX structure in REIGN-Streamer-Pro by removing redundant elements.

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,10 @@
 "use client"
+import { Silkscreen} from "next/dist/compiled/@next/font/dist/google";
 import "./globals.css";
-
+export  const silkscreen = Silkscreen
 export default function RootLayout({ children }) {
   return (
-    <html data-theme={`dark`} lang="en">
+    <html data-theme={`dark`} lang="en" className={silkscreen}>
       <body className={` antialiased`}>
         {children}
       </body>
--- components/REIGN-Streamer-Pro.jsx
+++ components/REIGN-Streamer-Pro.jsx
@@ -9,20 +9,26 @@ export default function ReignStreamerPro() {
     const [power, setPower] = React.useState(false);
     const [playing, setPlaying] = React.useState(false);
     return (
-        <div className={`border-t-[#191919] w-full border-b-[#434343]  p-3 inset-shadow-sm inset-shadow-black flex justify-around items-center bg-gradient-to-r from-[#1b1b22] to-[#271f1f]`}>
-            <div><PurpleRingLoader power={power}/></div>
-            <div></div>
-            <div>{power}</div>
-
-            <div className={`flex justify-center items-center gap-8`}>
-                <div className={`opacity-40`}><Heart size={27}/></div>
-                <div className={`opacity-40`}><SkipBack size={27}/></div>
+        <div
+            className={`border-t-[#191919] w-full border-b-[#434343]  px-6 py-4 inset-shadow-sm inset-shadow-black flex justify-between items-center bg-gradient-to-r from-[#0f0f17] to-[#100b0b]`}>
+            {/*<div><PurpleRingLoader power={power}/></div>*/}
+            {/*<div className={`p-3 flex border opacity-70 border-r-green-900 justify-center border-b-green-900 bg-gradient-to-tr border-t-0 from-green-700/11  inset-shadow-black inset-5 shadow-lg to-green-600/20 rounded w-44 text-center`}>*/}
+            {/*    <div className={`text-white text-center`}>REIGN PRO</div>*/}
+            {/*</div>*/}
+            <div className="lcd-screen">
+                <div className="lcd-content no-line-break w-full ml-10 silkscreen-regular">
+                    August 11, 2023
+                </div>
+            </div>
+            <div className={`flex justify-center items-center gap-4`}>
+                {/*<div className={`opacity-40`}><Heart size={27}/></div>*/}
+                <div className={`opacity-[0.05]`}><SkipBack size={45}/></div>
                 <PlayPauseBtn playing={playing} setPlaying={setPlaying} power={power}/>
-                <div className={`opacity-40`}><SkipForward size={27}/></div>
-                <div className={`opacity-40`}><Playlist size={27}/></div>
+                <div className={`opacity-[0.05]`}><SkipForward size={45}/></div>
+                {/*<div className={`opacity-40`}><Playlist size={27}/></div>*/}
             </div>
 
-            <div>{playing.toString()}</div>
+            {/*<div>{playing.toString()}</div>*/}
             <div></div>
             <div></div>
             <div><PowerToggle setPower={setPower} setPlaying={setPlaying}/></div>
